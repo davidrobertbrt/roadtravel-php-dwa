3e5dc49555e10671dcdf66681dc3d5c96a959a35
davidrobertbrt <84999955+davidrobertbrt@users.noreply.github.com>
2023-05-15 21:33:54 +0300

    Bus repo added and initial page

---
+++ app/model/Bus.php
@@ -0,0 +1,49 @@
+<?php
+
+class Bus
+{
+    private $id;
+    private $nrSeats;
+
+    public function __construct($id,$nrSeats)
+    {
+        $this->id = $id;
+        $this->nrSeats = $nrSeats;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nrSeats
+     */
+    public function getNrSeats()
+    {
+        return $this->nrSeats;
+    }
+
+    /**
+     * Set the value of nrSeats
+     */
+    public function setNrSeats($nrSeats): self
+    {
+        $this->nrSeats = $nrSeats;
+
+        return $this;
+    }
+}
---
+++ app/views/BusIndex.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <?php require_once('../app/components/header.php');?>
+    </head>
+    <body>
+        <?php require_once('../app/components/navbar.php');?>
+        <div class = "container mt-4">
+            <h1>Welcome to my website!</h1>
+            <p>This is a simple webpage made using Bootstrap.</p>
+        </div>
+        <?php require_once('../app/components/footer.php');?>
+    </body>
+</html>
