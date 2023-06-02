c8dea06b89338f8347c37079d9dcaeea5c6141e9
davidrobertbrt <84999955+davidrobertbrt@users.noreply.github.com>
2023-06-02 20:15:33 +0300

    added navbar functionality

--- app/components/navbar.php
+++ app/components/navbar.php
@@ -6,14 +6,49 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="./../home/index">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="./../booking/index">Booking</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="./../tickets/index">Tickets</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../bus/index">Bus list</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../trip/index">Trip</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../discount/index">Discounts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../location/index">Locations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../contact/index">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./../user/index">Users</a>
+                </li>
+                <li class="nav-item">
+
+                </li>
+                <li class="nav-item">
+                   
+                </li>
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Conectat ca...
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="./../profile/index">Edit profile</a>
+                        <a class="dropdown-item" href="./../logout/process">Disconnect</a>
+                    </div>
+                </li>
+                </ul>
             </ul>
         </div>
 </nav>
